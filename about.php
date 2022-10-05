6d8573df412be9013efb903ac9f9e8c1b532c482
charisnarizqi <irgienno@gmail.com>
2022-10-05 23:04:09 +0700

    tugas6

---
+++ about.php
@@ -0,0 +1,51 @@
+<section id="about" class="about">
+    <div class="container">
+
+        <div class="section-title">
+            <h2>About</h2>
+            <p>MY profil.</p>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-4" data-aos="fade-right">
+                <img src="assets/img/aayayayy.jpg" class="img-fluid" alt="">
+            </div>
+            <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+                <h3>Charisna Rizqi  Enno Firgian.</h3>
+                <p class="fst-italic">
+                    Tentang Saya:
+                </p>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <ul>
+                            <li><i class="bi bi-chevron-right"></i> <strong>Tanggal Lahir:</strong> <span>17 Apri 2002</span>
+                            </li>
+                            <li><i class="bi bi-chevron-right"></i> <strong>Gitub:</strong>
+                                <span>https://github.com/charisnarizqi</span>
+                            </li>
+                            <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>0859503256</span>
+                            </li>
+                            <li><i class="bi bi-chevron-right"></i> <strong>Kota:</strong> <span>Pati, Semarang</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-6">
+                        <ul>
+                            <li><i class="bi bi-chevron-right"></i> <strong>Umur:</strong> <span>20</span></li>
+                            
+                            <li><i class="bi bi-chevron-right"></i> <strong>Email
+                                <span>Irgienno@gmaail.com</span>
+                            </li>
+                           
+                        </ul>
+                    </div>
+                </div>
+                <p>
+                    Hallo perkenalkan saya Charisna Rizqi Enno Firgian, saya adalah mahassiswa semester 5 di Universitas Dian Nuswantoro.
+                    Saya di semester ini mengikuti Studi Independen di salah satu mitra yaitu Nurul Fikri Computer selama 5 Bulan.
+                </p>
+            </div>
+        </div>
+
+    </div>
+</section>
---
+++ study.php
@@ -0,0 +1,61 @@
+<section id="resume" class="resume">
+      <div class="container">
+        <div class="section-title">
+          <h2>Resume</h2>
+          <p>I have an interest as a cyber security, web and mobile developer</p>
+        </div>
+        <div class="row">
+          <div class="col-lg-6" data-aos="fade-up">
+            <h3 class="resume-title">Sumary</h3>
+            <div class="resume-item pb-0">
+              <h4>Charisna Rizqi Enno Firgian</h4>
+              <p><em>here are some ways to contact me : </em></p>
+              <ul>
+                <li>pati, Semarang
+                </li>
+                <li>08595032556</li>
+                <li>irgienno@gmail.com</li>
+              </ul>
+            </div>
+            <h3 class="resume-title">Education</h3>
+            <div class="resume-item">
+              <h4>Computer &amp; Network Engineering</h4>
+              <h5>2017 - 2020</h5>
+              <p><em>SMA negeri 1 tayu</em></p>
+              <p>Here I can assemble a computer,
+                reinstall Linux and Windows OS,
+                learn about Mikrotik,
+                 one of which is about client and server</p>
+            </div>
+            <div class="resume-item">
+              <h4> Informatics </h4>
+              <h5>2020 - Present</h5>
+              <p><em>Universitas Dian Nuswantoro</em></p>
+              <p>Here I can assemble a computer,
+                install Linux and Windows OS,
+                learn about Mikrotik,
+                 one of which is about client and server</p>
+            </div>
+          </div>
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+            <h3 class="resume-title">Professional Experience</h3>
+            <div class="resume-item">
+              <h4>Web Developer</h4>
+              <h5>2019 - Present</h5>
+              <p><em>Bekasi,Indonesia</em></p>
+              <ul>
+                <li>Building a Profile Company PT.Ganda Putra Ellonindo</li>
+              </ul>
+            </div>
+            <div class="resume-item">
+              <h4>Another Experience </h4>
+              <h5>2016 - 2021</h5>
+              <ul>
+                <li>PART-TIME CONVENIENCE STORE CASHIER <span class="badge rounded-pill bg-primary">January 2016 - December 2018</span></li>
+                <li>PART-TIME BARISTA CoffeeShop <span class="badge rounded-pill bg-primary">June 2019 - July 2020</span></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section><!-- End Resume Section -
